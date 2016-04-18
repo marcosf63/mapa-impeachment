a644b9b1d2c21085290735f3728da5394ac8d666
Marcos Oliveira <marcosf53@gmail.com>
2016-04-18 17:08:39 -0300

    Update

---
+++ congresso.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import json
+
+
+def lista_deputados():
+  retutn json.loads(open('mapa-congresso.json', 'w').read())
+
+def deputados_afavor_R(lista):
+  l = []
+  for i in range(len(lista)):
+    if 
+  
+
+def deputados_afavor_UF(uf):
+  pass
+
+
